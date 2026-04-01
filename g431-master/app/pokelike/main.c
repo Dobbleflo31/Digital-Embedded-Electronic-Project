12a403e355e7d4cbde20bb944e2f7463cd733f50
mchat450 <maximchatillon@icloud.com>
2026-04-01 10:55:42 +0200

    inclusion dans main de pokelike

--- g431-master/app/pokelike/main.c
+++ g431-master/app/pokelike/main.c
@@ -3,10 +3,21 @@
  *
  *  Created on: 27 mars 2026
  *      Author: maxim
- *      bonjour
+ *
  */
+#include "pokelike/main.h"
+
+#include "peripheriques/button.h"
+#include "peripheriques/display.h"
+#include <stdio.h>
+#include "tft_ili9341/stm32g4_ili9341.h"
+#include "tft_ili9341/stm32g4_fonts.h"
+#include "carte.h"
 
 void main_POKELIKE(void){
+	BUTTONS_init();
+	DISPLAY_init();
+
 
 	while(1){
 
